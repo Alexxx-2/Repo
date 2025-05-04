373b488296bd244e35237b4c665240929556c307
Alexxx-2 <alex.muinde@strathmore.edu>
2025-05-04 21:27:36 +0300

    leapyr.py

--- leapyr.py
+++ leapyr.py
@@ -1,6 +1,7 @@
-current_year=int(input("which year do you want to check:"))
+for i in range(5):
+    current_year = int(input("Which year do you want to check: "))
 
-if(current_year %4 == 0)and (current_year %4 != 100) or (current_year%400 == 0):
-    print("The year is a leap year")
-else:
-   print("Not a leap year")
+    if (current_year % 4 == 0 and current_year % 100 != 0) or (current_year % 400 == 0):
+        print("The year is a leap year")
+    else:
+        print("Not a leap year")
