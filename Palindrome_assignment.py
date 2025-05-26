2327508ef1cabe8e978109c4fe6b73ff4ba181f2
Alexxx-2 <alex.muinde@strathmore.edu>
2025-05-26 07:59:41 +0300

    commit

---
+++ Palindrome_assignment.py
@@ -0,0 +1,52 @@
+from collections import deque
+
+
+class Solution:
+    def __init__(self):
+        self.stack = []
+        self.queue = deque()
+
+    def push_character(self, ch):
+        self.stack.append(ch)
+
+    def enqueue_character(self, ch):
+        self.queue.append(ch)
+
+    def pop_character(self):
+        return self.stack.pop()
+
+    def dequeue_character(self):
+        return self.queue.popleft()
+
+
+def check_palindrome(s):
+    obj = Solution()
+
+    for char in s:
+        obj.push_character(char)
+        obj.enqueue_character(char)
+
+    is_palindrome = True
+    for _ in range(len(s)):
+        if obj.pop_character() != obj.dequeue_character():
+            is_palindrome = False
+            break
+
+    if is_palindrome:
+        return f"The word, '{s}', is a palindrome."
+    else:
+        return f"The word, '{s}', is not a palindrome."
+
+
+def main():
+    s = input(" Enter a word to check if it is a palindrome: ").strip()
+
+    if not s.isalpha():
+        print(" Please enter a valid word.")
+        return
+
+    print(check_palindrome(s.lower()))
+
+
+if __name__ == "__main__":
+    main()
